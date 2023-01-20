06c77a91455c3630e580ceb957f6a5542c85e633
王韋鈞 <weijyun0223@gmail.com>
2023-01-20 21:13:46 +0800

    finish project 7

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,25 @@
-//using selectors inside the element
-// traversing the dom
+// Solution 1: using selectors inside the element
+const questions = document.querySelectorAll('.question');
+
+questions.forEach(function(question){
+    const btn = question.querySelector('.question-btn');
+    btn.addEventListener('click', function(){
+        questions.forEach(function(item){
+            if(item !== question){
+                item.classList.remove("show-text");
+            }
+        });
+        question.classList.toggle("show-text");
+    });
+});
+
+
+// Solution 2: traversing the dom
+// const btns = document.querySelectorAll('.question-btn');
+
+// btns.forEach(function(btn){
+//     btn.addEventListener('click', function(e){
+//         const question = e.currentTarget.parentElement.parentElement;
+//         question.classList.add("show-text");
+//     });
+// });
